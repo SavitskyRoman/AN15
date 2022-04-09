968077b2e2495478c166e677d189d8aea6d81b89
SavitskyRoman <savitskyroman508@gmail.com>
2022-04-09 09:14:09 +0300

    hw11 started

---
+++ hw11/task1/model/Sex.java
@@ -0,0 +1,6 @@
+package com.teachMeSkills.an15.SavitskyRoman.hw11.task1.model;
+
+public enum Sex {
+    MAN, WOMEN;
+}
+
---
+++ hw11/task1/service/ConscriptService.java
@@ -0,0 +1,5 @@
+package com.teachMeSkills.an15.SavitskyRoman.hw11.task1.service;
+
+public interface ConscriptService {
+
+}
